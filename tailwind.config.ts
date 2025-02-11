dbd1aafdf7fdf7543045598f8c270b53a93709c0
Henrique <henriqueferreira0320@gmail.com>
2025-02-11 11:09:17 -0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "gray-900": "#262626",
+        "gray-800": "#404040",
+        "gray-700": "#595959",
+        "gray-600": "#737373",
+        "gray-500": "#A6A6A6",
+        "gray-400": "#DDDDDD",
+        "yellow-300": "#FFEE4C",
+        "yellow-400": "#FFE700",
+        "yellow-500": "#CCB900",
+        "red-400": "#ff6467",
+        "red-500": "#fb2c36",
+        "green-400": "#05df72",
+        "green-500": "#00c951",
       },
     },
   },
